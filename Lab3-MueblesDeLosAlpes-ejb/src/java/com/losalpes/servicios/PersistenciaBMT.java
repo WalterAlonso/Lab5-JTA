a4438fe4f4885deb04e4562088acdf7fdbf54afe
WalterAlonso <wj.alonso@uniandes.edu.co>
2017-11-10 10:22:30 -0500

    se estan modificando las clases de persistencia

--- Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
@@ -5,17 +5,160 @@
  */
 package com.losalpes.servicios;
 
+import com.losalpes.entities.RegistroVenta;
+import com.losalpes.entities.TarjetaCreditoAlpes;
+import com.losalpes.excepciones.OperacionInvalidaException;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 /**
  *
  * @author WALTER
  */
 @Stateless
-@LocalBean
+@TransactionManagement(TransactionManagementType.BEAN)
 public class PersistenciaBMT {
 
+    @Resource
+    private UserTransaction userTransaction;
+
+    /**
+     * La entidad encargada de persistir en la base de datos
+     */
+    @PersistenceContext(unitName = "Lab3-MueblesDeLosAlpes-ejbPUOracle")
+    private EntityManager ventas;
+
+    /**
+     * La entidad encargada de persistir en la base de datos
+     */
+    @PersistenceContext(unitName = "Lab3-MueblesDeLosAlpes-ejbPUDerby")
+    private EntityManager tarjeta;
+
+    @PostConstruct
+    public void postConstruct() {
+
+    }
+
+    private void initTransaction() {
+        try {
+            userTransaction.begin();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void commitTransaction() {
+        try {
+            userTransaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void rollbackTransaction() {
+        try {
+            userTransaction.rollback();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void comprar(RegistroVenta venta) {
+        try {
+            initTransaction();
+            ventas.persist(venta);
+            DescontarCupoTarjeta(venta);
+            commitTransaction();
+            /*} catch (CupoInsuficienteException e) {
+        context.setRollbackOnly();*/
+        } catch (Exception e) { //TODO: quitarla cuando se implemente la excepcion            
+            rollbackTransaction();
+            //retornar los objetos a como estaban? no es necesario, porque cuando los consulte tendra lo mismo            
+        }
+    }
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    private void DescontarCupoTarjeta(RegistroVenta venta) throws Exception { //,CupoInsuficienteException
+        double valorTotal = venta.getProducto().getPrecio() * venta.getCantidad();
+        TarjetaCreditoAlpes tarjetaCredito = venta.getComprador().getTarjetaCreditoAlpes();
+        double saldoEnTargeta = tarjetaCredito.getCupo() - valorTotal;
+        tarjetaCredito.setCupo(saldoEnTargeta);
+        tarjeta.persist(tarjetaCredito);
+
+        if (tarjetaCredito.getCupo() < 0) {
+            //throw new CupoInsuficienteException();
+            throw new Exception();
+        }
+    }
+
+    /**
+     * metodos de persistencia normales *
+     */
+    /**
+     * Permite crear un objeto dentro de la persistencia del sistema.
+     *
+     * @param obj Objeto que representa la instancia de la entidad que se quiere
+     * crear.
+     */
+    public void create(Object obj) {
+        initTransaction();
+        ventas.persist(obj);
+        commitTransaction();
+    }
+
+    /**
+     * Permite modificar un objeto dentro de la persistencia del sistema.
+     *
+     * @param obj Objeto que representa la instancia de la entidad que se quiere
+     * modificar.
+     */
+    public void update(Object obj) {
+        ventas.merge(obj);
+    }
+
+    /**
+     * Permite borrar un objeto dentro de la persistencia del sistema.
+     *
+     * @param obj Objeto que representa la instancia de la entidad que se quiere
+     * borrar.
+     */
+    public void delete(Object obj) throws OperacionInvalidaException {
+        ventas.remove(obj);
+
+    }
+
+    /**
+     * Retorna la lista de todos los elementos de una clase dada que se
+     * encuentran en el sistema.
+     *
+     * @param c Clase cuyos objetos quieren encontrarse en el sistema.
+     * @return list Listado de todos los objetos de una clase dada que se
+     * encuentran en el sistema.
+     */
+    public List findAll(Class c) {
+        return ventas.createQuery("select O from " + c.getSimpleName() + " as O").getResultList();
+    }
+
+    /**
+     * Retorna la instancia de una entidad dado un identificador y la clase de
+     * la entidadi.
+     *
+     * @param c Clase de la instancia que se quiere buscar.
+     * @param id Identificador unico del objeto.
+     * @return obj Resultado de la consulta.
+     */
+    public Object findById(Class c, Object id) {
+        return ventas.find(c, id);
+    }
 }
--- Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaCMT.java
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaCMT.java
@@ -7,6 +7,8 @@ package com.losalpes.servicios;
 
 import com.losalpes.entities.RegistroVenta;
 import com.losalpes.entities.TarjetaCreditoAlpes;
+import com.losalpes.excepciones.OperacionInvalidaException;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
@@ -55,13 +57,14 @@ public class PersistenciaCMT {
             /*} catch (CupoInsuficienteException e) {
         context.setRollbackOnly();*/
         } catch (Exception e) { //TODO: quitarla cuando se implemente la excepcion
-            context.setRollbackOnly();
+            context.setRollbackOnly(); //TODO: posiblemente no se nevcesite poner, porque cuando cree la excepcion la marca como 
+            //Rollbackk=true en su declaracion.
         }
     }
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 
-    private void DescontarCupoTarjeta(RegistroVenta venta) throws Exception {
+    private void DescontarCupoTarjeta(RegistroVenta venta) throws Exception  { //,CupoInsuficienteException
         double valorTotal = venta.getProducto().getPrecio() * venta.getCantidad();
         TarjetaCreditoAlpes tarjetaCredito = venta.getComprador().getTarjetaCreditoAlpes();
         double saldoEnTargeta = tarjetaCredito.getCupo() - valorTotal;
@@ -72,6 +75,57 @@ public class PersistenciaCMT {
             //throw new CupoInsuficienteException();
             throw new Exception();
         }
+    }
+    
+    /** metodos de persistencia normales **/
+    /**
+     * Permite crear un objeto dentro de la persistencia del sistema.
+     * @param obj Objeto que representa la instancia de la entidad que se quiere crear.
+     */    
+    public void create(Object obj)
+    {
+      ventas.persist(obj);
+    }
+
+    /**
+     * Permite modificar un objeto dentro de la persistencia del sistema.
+     * @param obj Objeto que representa la instancia de la entidad que se quiere modificar.
+     */
+    public void update(Object obj)
+    {
+       ventas.merge(obj);
+    }
+
+    /**
+     * Permite borrar un objeto dentro de la persistencia del sistema.
+     * @param obj Objeto que representa la instancia de la entidad que se quiere borrar.
+     */
+    public void delete(Object obj) throws OperacionInvalidaException
+    {
+        ventas.remove(obj);
+
+    }
+
+    /**
+     * Retorna la lista de todos los elementos de una clase dada que se encuentran en el sistema.
+     * @param c Clase cuyos objetos quieren encontrarse en el sistema.
+     * @return list Listado de todos los objetos de una clase dada que se encuentran en el sistema.
+     */
+    public List findAll(Class c)
+    {
+
+        return ventas.createQuery("select O from " + c.getSimpleName() + " as O").getResultList();
+    }
+
+    /**
+     * Retorna la instancia de una entidad dado un identificador y la clase de la entidadi.
+     * @param c Clase de la instancia que se quiere buscar.
+     * @param id Identificador unico del objeto.
+     * @return obj Resultado de la consulta.
+     */
+    public Object findById(Class c, Object id)
+    {
+     return ventas.find(c, id);
     }
 
 }
